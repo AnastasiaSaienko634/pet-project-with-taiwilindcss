21a6b0e2d670cdc1977129c163b1ba5ee8bd8571
nasti <anastasiasaienko634@gmail.com>
2025-06-29 00:34:55 +0200

    update all

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,10 @@ export function Home() {
       {" "}
       <div className="container bg-white w-80 h-160 mx-auto mt-10 sm:mt:2 rounded-2xl  ">
         <header className=" flex h-16 bg-white rounded-t-2xl">
-          <h1 className="flex px-5 py-5 text-25 font-medium cursor-pointer ">
-            <button className="cursor-pointer">
-              <SlArrowLeft className="w-5 h-5 mx-2 fill-gray-400 cursor-pointer " />
-            </button>
+          <button className=" flex px-5 py-5 items-center text-25 font-medium cursor-pointer">
+            <SlArrowLeft className="w-5 h-5 mx-2 fill-gray-400 cursor-pointer " />
             Wallet
-          </h1>
+          </button>
         </header>
         <Balance />
         <PaymentCards />
